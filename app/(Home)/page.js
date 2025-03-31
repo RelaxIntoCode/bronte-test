595d6461a32442e65218b66aaac3702a89c35006
RelaxIntoCode <jayow1991@gmail.com>
2025-04-01 10:31:12 +1100

    Add Footer section initial styling

--- app/(Home)/page.js
+++ app/(Home)/page.js
@@ -3,6 +3,7 @@ import AboutSection from "@/components/AboutSection/AboutSection";
 import styles from "./page.module.css";
 import Link from "next/link";
 import ServicesSection from "@/components/ServicesSection/ServicesSection";
+import Footer from "@/components/Footer/Footer";
 export default function Home() {
   return (
     <div className={styles.page}>
@@ -11,6 +12,7 @@ export default function Home() {
       </div>
       <AboutSection />
       <ServicesSection />
+      <Footer />
     </div>
   );
 }
---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,29 @@
+import styles from "./Footer.module.css";
+import { FaLocationDot } from "react-icons/fa6";
+import { GiRotaryPhone } from "react-icons/gi";
+import { FaRegCopyright } from "react-icons/fa6";
+
+function Footer() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.contentContainer}>
+        <h3>Contact Us</h3>
+        <div>
+          <div className={styles.contactContainer}>
+            <FaLocationDot />
+            <p>Northern Beaches, Sydney</p>
+          </div>
+          <div className={styles.contactContainer}>
+            <GiRotaryPhone />
+            <p>0412 345 678</p>
+          </div>
+          <div className={styles.contactContainer}>
+            <FaRegCopyright />
+            <p>Copyright. Relax Into Code. All Rights Reserved 2025</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Footer;
